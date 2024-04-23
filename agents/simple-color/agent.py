75f81c63d7e7d487a4545068261474dd67c7f3c9
joseph.kieu <joseph.kieu@bluebelt.asia>
2024-04-23 11:16:41 +0700

    fix

--- agents/simple-color/agent.py
+++ agents/simple-color/agent.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 import logging
 import random
 
@@ -21,33 +22,6 @@ r = redis.Redis(host="127.0.0.1", port=6378, decode_responses=True)
 sub = r.pubsub()
 sub.subscribe("loki")
 
-# change this color in dev mode and the agent will automatically update
-# COLOR = bytes([0, 255, 255, 255])
-
-
-# async def entrypoint(job: JobContext):
-#     room = job.room
-#     video_capture = cv2.VideoCapture("/Users/tuankq/Downloads/C0509.mp4")
-#     source = rtc.VideoSource(WIDTH, HEIGHT)
-#     while True:
-#         ret, frame = video_capture.read()
-#         if not ret:
-#             break
-#         # Resize the frame to match the desired width and height
-#         frame = cv2.resize(frame, (WIDTH, HEIGHT))
-#         # Convert the frame to RGB format
-#         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-#         # Convert the frame to bytes
-#         byte_frame = rgb_frame.tobytes()
-#         # Create a VideoFrame object
-#         video_frame = rtc.VideoFrame(WIDTH, HEIGHT, rtc.VideoBufferType.RGBA, byte_frame)
-#         # Pass the frame to the VideoSource object
-#         source.capture_frame(video_frame)
-#         # Delay to control the frame rate (optional)
-#         await asyncio.sleep(0.1)
-#     video_capture.release()
-
-
 async def entrypoint(job: JobContext):
     room = job.room
     source = rtc.VideoSource(WIDTH, HEIGHT)
@@ -57,13 +31,30 @@ async def entrypoint(job: JobContext):
     logging.info("published track", extra={"track_sid": publication.sid})
 
     async def _draw_color():
-        video_capture = cv2.VideoCapture("/Users/tuankq/Downloads/C0509.mp4")
-
+        video_source_capture = cv2.VideoCapture("/home/tuankq/workspaces/livekit-demo/server/agents/simple-color/C0492.mp4")
+        video_synced_capture = cv2.VideoCapture("/home/tuankq/workspaces/livekit-demo/server/agents/simple-color/synced.mp4")
+        # Get the total number of frames in the video
+        total_frames = int(video_source_capture.get(cv2.CAP_PROP_FRAME_COUNT))
+        logging.info(f"Total frames in video: {total_frames}")
+        synced_frame_count = 0
         while True:
             msg = sub.get_message()
-            if msg:
-                logging.info(msg)
-            _, frame = video_capture.read()
+            if msg is not None:
+                if "data" in msg and isinstance(msg["data"], str):
+                    logging.info(msg)
+                    total_seconds = json.loads(msg["data"]).get("total_seconds")
+                    logging.info(f"Total seconds: {total_seconds}")
+                    synced_frame_count = int(total_seconds * 25)
+            logging.info(f"synced_frame_count: {synced_frame_count}")
+            if synced_frame_count > 0:
+                ret, frame = video_synced_capture.read()
+                synced_frame_count = synced_frame_count - 1
+            else:
+                ret, frame = video_source_capture.read()
+            if not ret:
+                logging.info("End of video reached. Jumping to start...")
+                video_source_capture.set(cv2.CAP_PROP_POS_FRAMES, 0)
+                ret, frame = video_source_capture.read()
             argb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
             argb_frame = argb_frame.tobytes()
             frame = rtc.VideoFrame(WIDTH, HEIGHT, rtc.VideoBufferType.RGBA, argb_frame)
