52aadf048cb650d5502135539047990a6715429e
Tuan Kieu <joseph.kieu@bluebelt.asia>
2024-05-02 15:37:37 +0700

    fiox

--- src/agent.py
+++ src/agent.py
@@ -9,12 +9,10 @@ import traceback
 import uuid
 import wave
 
-import audioread
 
 import livekit.api.room_service
 import numpy as np
 import redis
-import soundfile
 from dotenv import load_dotenv
 from livekit.protocol import agent
 from livekit.rtc import AudioFrame
@@ -39,6 +37,10 @@ WIDTH = 540
 HEIGHT = 960
 r = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=0)
 
+AUDIO_SAMPLE_RATE = 24000
+AUDIO_CHANNEL_NUMBERS = 1
+
+
 async def capture_and_send_audio(audio_source, audio_file):
     with wave.open(audio_file, "rb") as wf:
         num_channels = wf.getnchannels()
@@ -58,21 +60,25 @@ async def capture_and_send_audio(audio_source, audio_file):
     await audio_source.capture_frame(audio_frame)
 
 
+def calculate_samples_per_channel(raw_bytes_length, sample_rate):
+    # Calculate total number of samples
+    total_samples = (
+        raw_bytes_length // 2
+    )  # Each sample is represented by 2 bytes (16 bits)
+    # Calculate duration in seconds
+    duration_seconds = total_samples / sample_rate
+    samples_per_channel = sample_rate * duration_seconds
+    return int(samples_per_channel)
+
+
 async def capture_and_send_audio_from_binary(audio_source, audio_bytes, count):
-    audio_segment = AudioSegment.from_file(io.BytesIO(audio_bytes), format="mp3")
-    # Export the AudioSegment to WAV format
-    wav_bytes = audio_segment.export(format="wav", codec="pcm_s16le").read()
-    with wave.Wave_read(io.BytesIO(wav_bytes)) as wf:
-        num_channels = wf.getnchannels()
-        sample_width = wf.getsampwidth()
-        sample_rate = wf.getframerate()
-        num_frames = wf.getnframes()
-        pcm_data = wf.readframes(num_frames)
     audio_frame = AudioFrame(
-        data=pcm_data,
-        sample_rate=sample_rate,
-        num_channels=num_channels,
-        samples_per_channel=num_frames,
+        data=audio_bytes,
+        sample_rate=AUDIO_SAMPLE_RATE,
+        num_channels=AUDIO_CHANNEL_NUMBERS,
+        samples_per_channel=calculate_samples_per_channel(
+            len(audio_bytes), AUDIO_SAMPLE_RATE
+        ),
     )
     #
     await audio_source.capture_frame(audio_frame)
@@ -110,9 +116,11 @@ async def entrypoint(job: JobContext):
                 if msg is not None:
                     data = msg["data"]
                     if isinstance(data, bytes):
-                        await capture_and_send_audio_from_binary(audio_source, data, count)
+                        await capture_and_send_audio_from_binary(
+                            audio_source, data, count
+                        )
                         count = count + 1
-                await asyncio.sleep(0.000000001)
+                await asyncio.sleep(0.04)
         except:
             print(f"Error: {traceback.format_exc()}")
         #         # if msg['type'] == 'message':
@@ -130,6 +138,8 @@ async def entrypoint(job: JobContext):
         #         audio_file = data.get("audio_file")
 
     async def _publish_frame():
+        sub = r.pubsub()
+        sub.subscribe(f"lip:synced:{str(job.room.name)}")
         logging.info(f"Subscribed to {job.room.name}")
         video_source_capture = cv2.VideoCapture(SOURCE_VIDEO)
         video_synced_capture = cv2.VideoCapture(LIPSYNCED_VIDEO)
@@ -141,6 +151,20 @@ async def entrypoint(job: JobContext):
         total_frames = min(total_source_frames, total_synced_frames)
         synced_frame_count = 0
         while True:
+            msg = sub.get_message()
+            if msg is not None:
+                logging.info(f">>>>>>>{msg}")
+                if "data" in msg and isinstance(msg["data"], bytes):
+                    data = json.loads(msg["data"])
+                    logging.info(data)
+                    if data.get("total_seconds"):
+                        logging.info(msg)
+                        total_seconds = data.get("total_seconds")
+                        logging.info(f"Total seconds: {total_seconds}")
+                        synced_frame_count = synced_frame_count + int(
+                            round(total_seconds * 25)
+                        )
+                        logging.info(f"synced_frame_count: {synced_frame_count}")
             src_ret, source_frame = video_source_capture.read()
             syn_ret, synced_frame = video_synced_capture.read()
 
@@ -150,12 +174,11 @@ async def entrypoint(job: JobContext):
                 video_synced_capture.set(cv2.CAP_PROP_POS_FRAMES, 0)
                 _, source_frame = video_source_capture.read()
                 _, synced_frame = video_synced_capture.read()
-            # if synced_frame_count > 0:
-            #     frame = synced_frame
-            #     synced_frame_count = synced_frame_count - 1
-            # else:
-            #     frame = source_frame
-            frame = source_frame
+            if synced_frame_count > 0:
+                frame = synced_frame
+                synced_frame_count = synced_frame_count - 1
+            else:
+                frame = source_frame
             argb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
             argb_frame = argb_frame.tobytes()
             frame = rtc.VideoFrame(WIDTH, HEIGHT, rtc.VideoBufferType.RGBA, argb_frame)
